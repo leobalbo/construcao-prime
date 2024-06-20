3e461fd0bc22aaa498b422544b907ee144c4032e
leobalbo <leobalbo7@gmail.com>
2024-06-20 10:44:08 -0300

    💄 feat: Efeito de transição

---
+++ src/components/utils/transition.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { motion } from 'framer-motion'
+
+export default function Transition({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
