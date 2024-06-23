d5eae260d8544af76caa849314a6fabb83881d6b
leobalbo <leobalbo7@gmail.com>
2024-06-23 01:51:59 -0300

    🐛fix: import checkRole

--- src/app/(pages)/admin/dashboard/_actions.ts
+++ src/app/(pages)/admin/dashboard/_actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { checkRole } from '@/utils/roles'
+import { checkRole } from '@/lib/user'
 import { clerkClient } from '@clerk/nextjs/server'
 
 export async function setRole(formData: FormData) {
